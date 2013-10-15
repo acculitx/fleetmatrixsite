6d5084eb7996db3a9bcd01cb5a79df8d3352b4b6
maulik <maulik@09b8f181-b358-4790-9798-8c328cd25aca>
2013-10-15 18:39:45 +0000

    added redflag reporting

--- trunk/components/com_fleetmatrix/views/reports/tmpl/default_driver_body.php
+++ trunk/components/com_fleetmatrix/views/reports/tmpl/default_driver_body.php
@@ -6,6 +6,42 @@ defined('_JEXEC') or die('Restricted Access');
 <?php foreach($this->items as $i => $item): ?>
 
 	<tr class="row<?php echo $i % 2; ?>">
+		<td style="font-weight: bold; font-size: 15px;">
+			<?php if($item->decel_scoretype == 0) {
+				echo "<span style='color: #f5e100;'>";
+			} else if($item->decel_scoretype == 1) {
+				echo "<span style='color: #ffa500;'>";
+			} else { 
+				echo "<span style='color: #ff0000;'>";
+			} 
+					echo ($item->decel_count == 0)?'':$item->decel_count;
+				echo "</span>";
+			?>
+		</td>
+		<td style="font-weight: bold; font-size: 15px;">
+			<?php if($item->accel_scoretype == 0) {
+				echo "<span style='color: #f5e100;'>";
+			} else if($item->accel_scoretype == 1) {
+				echo "<span style='color: #ffa500;'>";
+			} else { 
+				echo "<span style='color: #ff0000;'>";
+			} 
+					echo ($item->accel_count == 0)?'':$item->accel_count;
+				echo "</span>";
+			?>
+		</td>
+		<td style="font-weight: bold; font-size: 15px;">
+			<?php if($item->hard_turns_scoretype == 0) {
+				echo "<span style='color: #f5e100;'>";
+			} else if($item->hard_turns_scoretype == 1) {
+				echo "<span style='color: #ffa500;'>";
+			} else { 
+				echo "<span style='color: #ff0000;'>";
+			} 
+					echo ($item->hard_turns_count == 0)?'':$item->hard_turns_count;
+				echo "</span>";
+			?>
+		</td>
         <td><a class="map-link" href="/component/fleetmatrix?view=map&trip=<?php echo $item->trip_id; ?>&tmpl=component">Map</a></td>
 		<td>
 			<?php echo $item->vehicle_name; ?>
--- trunk/components/com_fleetmatrix/views/reports/tmpl/default_driver_head.php
+++ trunk/components/com_fleetmatrix/views/reports/tmpl/default_driver_head.php
@@ -8,17 +8,29 @@ include(JPATH_COMPONENT . DS . 'models' . DS . 'driver_search_controls.php');
 <script src="/colorbox/colorbox/jquery.colorbox-min.js"></script>
 
 <link rel="stylesheet" href="/colorbox/example1/colorbox.css" />
+<tr style="background: #f7f7f7; border: none;">
+	<td colspan="3" style="text-align: center; color: red;">Red Flags</td>
+</tr>
 <tr>
+	<th style="font-size: 10px;">
+        <?php echo JText::_('Brake'); ?>
+    </th>
+	<th style="font-size: 10px;">
+        <?php echo JText::_('Accel'); ?>
+    </th>
+	<th style="font-size: 10px;">
+        <?php echo JText::_('Turns'); ?>
+    </th>
     <th>
         &nbsp;
     </th>
-	<th>
+	<th style="width: 200px;">
         <?php echo JText::_('Vehicle Name'); ?>
     </th>
-    <th>
+    <th style="width: 150px;">
 		<?php echo JText::_('Trip Start'); ?>
 	</th>
-	<th>
+	<th style="width: 150px;">
 		<?php echo JText::_('Trip End'); ?>
 	</th>
 	<th>
