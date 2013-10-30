60626130da99cb19e100afd9bc23ee012409c749
kelvin <kelvin@09b8f181-b358-4790-9798-8c328cd25aca>
2013-10-30 05:01:14 +0000

    Added new Idle Time report.

---
+++ components/com_fleetmatrix/views/reports/tmpl/default_idletime.php
@@ -0,0 +1,23 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted Access');
+
+// load tooltip behavior
+JHtml::_('behavior.tooltip');
+
+?>
+
+<h2>Reports - Idle Time Report</h2>
+<br />
+
+<form action="<?php echo JRoute::_($this->getRoute()); ?>" method="post" name="adminForm">
+
+	<table class="adminlist">
+		<thead><?php echo $this->loadTemplate('idletime_head');?></thead>
+		<tfoot><?php echo $this->loadTemplate('idletime_foot');?></tfoot>
+		<tbody><?php echo $this->loadTemplate('idletime_body');?></tbody>
+	</table>
+</form>
+
+<br />
+
---
+++ components/com_fleetmatrix/views/reports/tmpl/default_idletime_head.php
@@ -0,0 +1,23 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted Access');
+
+
+include(JPATH_COMPONENT . DS . 'models' . DS . 'search_controls.php');
+
+?>
+
+<tr>
+    <th>
+        <?php echo JText::_('Driver Name'); ?>
+    </th>
+	<th>
+        <?php echo JText::_('Idle Time (minutes)'); ?>
+    </th>
+    <th>
+		<?php echo JText::_('Miles Driven'); ?>
+	</th>
+	<th>
+		<?php echo JText::_('Number of trips'); ?>
+	</th>
+</tr>
