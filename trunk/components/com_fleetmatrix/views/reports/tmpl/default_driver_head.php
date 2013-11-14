5193271f37ee0ce62506381726a0322405e27a4f
kelvin <kelvin@09b8f181-b358-4790-9798-8c328cd25aca>
2013-11-14 07:27:27 +0000

    added textual notification for sortable columns

--- trunk/components/com_fleetmatrix/views/reports/tmpl/default_driver_head.php
+++ trunk/components/com_fleetmatrix/views/reports/tmpl/default_driver_head.php
@@ -18,34 +18,34 @@ include(JPATH_COMPONENT . DS . 'models' . DS . 'driver_search_controls.php');
 <p><font color="red"> Red Flags </font></p>
 
 <tr class = "sortable">
-	<th style="font-size: 10px; font-family: inherit; font-weight: bold;">
+	<th style="font-size: 10px; font-family: inherit; font-weight: bold;" title="Click to sort by this column" >
         <?php echo JText::_('Brake'); ?>
     </th>
-	<th style="font-size: 10px; font-family: inherit; font-weight: bold;">
+	<th style="font-size: 10px; font-family: inherit; font-weight: bold;" title="Click to sort by this column">
         <?php echo JText::_('Accel'); ?>
     </th>
-	<th style="font-size: 10px; font-family: inherit; font-weight: bold;">
+	<th style="font-size: 10px; font-family: inherit; font-weight: bold;" title="Click to sort by this column">
         <?php echo JText::_('Turns'); ?>
     </th>
     <th>
         &nbsp;
     </th>
-	<th style="width: 200px;">
+	<th style="width: 200px;" title="Click to sort by this column">
         <?php echo JText::_('Vehicle Name'); ?>
     </th>
-    <th style="width: 150px;">
+    <th style="width: 150px;" title="Click to sort by this column">
 		<?php echo JText::_('Trip Start'); ?>
 	</th>
-	<th style="width: 150px;">
+	<th style="width: 150px;" title="Click to sort by this column">
 		<?php echo JText::_('Trip End'); ?>
 	</th>
-	<th>
+	<th title="Click to sort by this column">
 		<?php echo JText::_('Miles Driven'); ?>
 	</th>
-    <th>
+    <th title="Click to sort by this column">
         <?php echo JText::_('Assigned Driver'); ?>
     </th>
-    <th>
+    <th title="Click to sort by this column">
         <?php echo JText::_('Idle Time'); ?>
     </th>
 </tr>
--- trunk/components/com_fleetmatrix/views/reports/tmpl/default_driverdetail_head.php
+++ trunk/components/com_fleetmatrix/views/reports/tmpl/default_driverdetail_head.php
@@ -8,31 +8,31 @@ include(JPATH_COMPONENT . DS . 'models' . DS . 'search_controls.php');
 ?>
 
 <tr class = "sortable">
-    <th>
+    <th title="Click to sort by this column">
         <?php echo JText::_('Driver Name'); ?>
     </th>
-	<th>
+	<th title="Click to sort by this column">
         <?php echo JText::_('Group Name'); ?>
     </th>
-    <th>
+    <th title="Click to sort by this column">
         <?php echo JText::_('Overall score'); ?>
     </th>
-    <th>
+    <th title="Click to sort by this column">
         <?php echo JText::_('Accelerations score'); ?>
     </th>
-    <th>
+    <th title="Click to sort by this column">
         <?php echo JText::_('Decelerations score'); ?>
     </th>
-    <th>
+    <th title="Click to sort by this column">
         <?php echo JText::_('Hard turns score'); ?>
     </th>
-    <th>
+    <th title="Click to sort by this column">
         <?php echo JText::_('Speed by street score'); ?>
     </th>
-    <th>
+    <th title="Click to sort by this column">
 		<?php echo JText::_('Miles Driven'); ?>
 	</th>
-	<th>
+	<th title="Click to sort by this column">
 		<?php echo JText::_('Number of trips'); ?>
 	</th>
 </tr>
