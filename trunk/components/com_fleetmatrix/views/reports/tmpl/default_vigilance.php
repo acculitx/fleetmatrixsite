a08f93228da3c803642b3630b72134056f3e69d4
kelvin <kelvin@09b8f181-b358-4790-9798-8c328cd25aca>
2013-11-24 10:09:06 +0000

    Added header and init stuff for vigilance report

---
+++ trunk/components/com_fleetmatrix/views/reports/tmpl/default_vigilance.php
@@ -0,0 +1,31 @@
+<?php
+ $document = JFactory::getDocument();
+ $document->addScript('sorttable.js');
+ 
+?>
+
+
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted Access');
+
+// load tooltip behavior
+JHtml::_('behavior.tooltip');
+
+
+
+?>
+
+<h2>Reports - Driver Vigilance Report (Events) </h2>
+<br />
+
+<form action="<?php echo JRoute::_($this->getRoute()); ?>" id="adminForm" method="post" name="adminForm">
+	<table id="vigilance_table" class="adminlist sortable">
+		<thead><?php echo $this->loadTemplate('vigilance_head');?></thead>
+		<tfoot><?php echo $this->loadTemplate('vigilance_foot');?></tfoot>
+		<tbody><?php echo $this->loadTemplate('vigilance_body');?></tbody>
+	</table>
+</form>
+
+<br />
+
