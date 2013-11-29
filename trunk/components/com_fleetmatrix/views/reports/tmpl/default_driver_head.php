9ca053743cc02fc42c0bbf0e02fe9136eb1d7229
kelvin <kelvin@09b8f181-b358-4790-9798-8c328cd25aca>
2013-11-29 09:59:12 +0000

    Finished the new change for trip report. Removed overall score from driver trend graph.

--- trunk/components/com_fleetmatrix/views/reports/tmpl/default_driver_head.php
+++ trunk/components/com_fleetmatrix/views/reports/tmpl/default_driver_head.php
@@ -12,40 +12,51 @@ include(JPATH_COMPONENT . DS . 'models' . DS . 'driver_search_controls.php');
 <script src="/colorbox/colorbox/jquery.colorbox-min.js"></script>
 
 <link rel="stylesheet" href="/colorbox/example1/colorbox.css" />
-
-<br><br> 
-
-<p><font color="red"> Red Flags </font></p>
-
-<tr class = "sortable">
-	<th style="font-size: 10px; font-family: inherit; font-weight: bold;" title="Click to sort by this column" >
-        <?php echo JText::_('Brake'); ?>
-    </th>
-	<th style="font-size: 10px; font-family: inherit; font-weight: bold;" title="Click to sort by this column">
-        <?php echo JText::_('Accel'); ?>
-    </th>
-	<th style="font-size: 10px; font-family: inherit; font-weight: bold;" title="Click to sort by this column">
-        <?php echo JText::_('Turns'); ?>
-    </th>
-    <th>
-        &nbsp;
-    </th>
-	<th style="width: 200px;" title="Click to sort by this column">
-        <?php echo JText::_('Vehicle Name'); ?>
-    </th>
-    <th style="width: 150px;" title="Click to sort by this column">
+ 
+<tr>
+    <th title="Click to sort by this column">
+		<?php echo JText::_('Driver Name'); ?>
+	</th>
+    <th title="Click to sort by this column">
+		<?php echo JText::_('Company Name'); ?>
+	</th>
+    <th title="Click to sort by this column">
+		<?php echo JText::_('Group Name'); ?>
+	</th>
+	<th title="Click to sort by this column">
 		<?php echo JText::_('Trip Start'); ?>
 	</th>
-	<th style="width: 150px;" title="Click to sort by this column">
-		<?php echo JText::_('Trip End'); ?>
+	<th title="Click to sort by this column">
+		<?php echo JText::_('Accel Hard'); ?>
+	</th>
+	<th title="Click to sort by this column">
+		<?php echo JText::_('Accel Severe'); ?>
+	</th>
+	<th title="Click to sort by this column">
+		<?php echo JText::_('Brake Hard'); ?>
+	</th>
+	<th title="Click to sort by this column">
+		<?php echo JText::_('Brake Severe'); ?>
+	</th>
+	<th title="Click to sort by this column">
+		<?php echo JText::_('Turn Hard'); ?>
+	</th>
+	<th title="Click to sort by this column">
+		<?php echo JText::_('Turn Severe'); ?>
+	</th>
+	<th title="Click to sort by this column">
+		<?php echo JText::_('Speed Hard'); ?>
+	</th>
+	<th title="Click to sort by this column">
+		<?php echo JText::_('Speed Severe'); ?>
 	</th>
 	<th title="Click to sort by this column">
 		<?php echo JText::_('Miles Driven'); ?>
 	</th>
-    <th title="Click to sort by this column">
-        <?php echo JText::_('Assigned Driver'); ?>
-    </th>
-    <th title="Click to sort by this column">
-        <?php echo JText::_('Idle Time'); ?>
-    </th>
+	<th title="Click to sort by this column">
+		<?php echo JText::_('Idle Time (min)'); ?>
+	</th>
+	<th title="Click to sort by this column">
+		<?php echo JText::_('Map Link'); ?>
+	</th>
 </tr>
