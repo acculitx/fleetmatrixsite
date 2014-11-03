18b74da644d22065f818f87da8df741ef08ee42e
rodney <rodney@09b8f181-b358-4790-9798-8c328cd25aca>
2014-11-03 01:42:29 +0000

    Fixed decimal degress conversion of latitude and longitude.

--- trunk/components/com_fleetmatrix/models/maplist.php
+++ trunk/components/com_fleetmatrix/models/maplist.php
@@ -55,7 +55,62 @@ class FleetMatrixModelMapList extends FleetMatrixModelBaseList
         //var_dump((string)$query);
 		return $query;
 	}
-	
+
+    protected function decimal2DMS($value) {
+        $value = $value / 100;
+        $deg = (int)($value);
+        $minpart = (60 * ($value - $deg));
+        $min = (int)($minpart);
+        $sec = round((60 * ($minpart - $min)), 4);
+        if ($sec == 60) {
+            $min += 1;
+            $sec = 0;
+        }
+        if ($min == 60) {
+            $deg += 1;
+            $min = 0;
+        }
+        return sprintf('%02d,%02d,%06.4f', $deg, $min, $sec);
+    }
+
+    protected function degrees2decimal($value) {
+        $deg = (int)($value);
+        $min = (($value - $deg) * 100) / 60;
+        $ret = sprintf('%6.9f', $deg + $min);
+        return $ret;
+    }
+
+    /**
+     * convertGPS 
+     * takes in lat and lon parameters as DM (degrees, minutes)
+     * converts this into DD (decimal degrees) 
+     * ex. of cooridates 3113.31750,N 8534.26810,W from fleet_gps table
+     * 3113.31750 = 31 degrees 13.3175 minutes
+     * converts to 31.222458 dd
+     * if lat_dir and lon_dir are S or W, then negate the direction
+     * 8534.26810 = 85 degrees 34.2681 minutes
+     * converts to -85.571135 dd
+     * (http://support.groundspeak.com/index.php?pg=kb.page&id=207)
+     * 
+     * @param float $lat
+     * @param string $lat_dir
+     * @param float $lon
+     * @param string $lon_dir
+     * @return string
+     */
+    protected function convertGPS($lat, $lat_dir, $lon, $lon_dir)
+    {        
+        $lon /= 100;
+        $lat /= 100;
+          
+        $lon = $this->degrees2decimal($lon);
+        $lat = $this->degrees2decimal($lat);
+          
+        $lat *= $lat_dir == "S" ? -1 : 1;
+        $lon *= $lon_dir == "W" ? -1 : 1;
+        return sprintf('%s,%s', $lon, $lat);
+    }
+
     public function getItems() {
         $result = array();
 
@@ -63,16 +118,17 @@ class FleetMatrixModelMapList extends FleetMatrixModelBaseList
         $db->setQuery($this->getListQuery());
         $items = &$db->loadObjectList();
 
-        foreach ($items as &$item) {        	
-        	$lat_dd = $item->lat_dd;
-        	$lon_dd = $item->lon_dd;
-        	
-        	$item->coordinates = sprintf('%s,%s', $lon_dd, $lat_dd);
-        	
-            if ($item->coordinates == "0.00000,0.00000") {
+        foreach ($items as &$item) {
+            $item->coordinates = $this->convertGPS(
+                $item->latitude,
+                $item->lat_dir,
+                $item->longitude,
+                $item->lon_dir
+            );
+            
+            if ($item->coordinates == "0,0") {
                 continue;
             }
-            
             if (array_key_exists($item->trip_id, $result)) {
                 $result[$item->trip_id][] = $item->coordinates;
             } else {
