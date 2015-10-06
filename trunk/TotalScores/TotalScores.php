7d18e530c06ab0ebd0decce7632d627a8f2c6f72
johndimm <johndimm@09b8f181-b358-4790-9798-8c328cd25aca>
2015-10-06 18:01:41 +0000

    Better query generation.

--- trunk/TotalScores/TotalScores.php
+++ trunk/TotalScores/TotalScores.php
@@ -11,9 +11,7 @@ $link = mysql_connect($mysql_host, $mysql_user, $mysql_password) or die('Could n
 mysql_select_db($mysql_database) or die('Could not select database');
 mysql_set_charset('utf8', $link);
 
-
 // Read query params.
-
 $company = $_GET['c'];
 $group   = $_GET['g'];
 $driver  = $_GET['d'];
@@ -21,12 +19,24 @@ $slice   = $_GET['s'];
 $t0      = $_GET['t0'];
 $t1      = $_GET['t1'];
 
+// Test values.
 //$company = "29";
+//$group = "*";
 //$group = "49";
 //$driver = "*";
-$company = "*";
-$t0 = "2015-04-01";
-$t1 = "2015-09-01";
+//$company = "*";
+//$t0 = "2015-04-01";
+//ß$t1 = "2015-09-01";
+
+// Define data source.
+$table             = "fleet_daily_total_score";
+$aggregate_columns = array(
+  //  "totalScore",
+  //  "aggressiveScore",
+  "distractionScore"
+);
+$date_column       = "date";
+$timeslice         = "%Y-%m";
 
 $request_columns = "";
 $where           = "";
@@ -44,21 +54,23 @@ if (isset($t1)) {
 
 if (isset($driver)) {
   $request_columns .= " driver.id as driver_id, driver.name as driver_name, ";
+  
   array_push($array_alias, "driver_id", "driver_name");
   array_push($array_groupby, "driver.id", "driver.name");
-
+  
   if ($driver != "*") {
     $where .= " AND driver.id=$driver ";
   }
 }
 
 if (isset($group)) {
-   $request_columns .= " dgroup.id as group_id, dgroup.name as group_name, ";
-
+  $request_columns .= " dgroup.id as group_id, dgroup.name as group_name, ";
+  
   if (count($array_alias) == 0)
     array_push($array_alias, "group_id", "group_name");
-
+  
   array_push($array_groupby, "dgroup.id", "dgroup.name");
+  
   if ($group != "*") {
     $where .= " AND dgroup.id=$group ";
   }
@@ -66,62 +78,74 @@ if (isset($group)) {
 
 if (isset($company)) {
   $request_columns .= " company.id as company_id, company.name as company_name, ";
-
+  
   if (count($array_alias) == 0)
     array_push($array_alias, "company_id", "company_name");
-
+  
   array_push($array_groupby, "company.id", "company.name");
+  
   if ($company != "*") {
     $where .= " AND company.id=$company ";
   }
 }
 
-$groupby       = join($array_groupby, ",");
-$alias_columns = join($array_alias, ",");
+$groupby_columns = join($array_groupby, ",");
+$alias_columns   = join($array_alias, ",");
 
 
-$table             = "fleet_daily_total_score";
-$group_by_columns  = $groupby;
-$aggregate_columns = array(
-  "totalScore",
-  "aggressiveScore",
-  "distractionScore"
-);
-$date_column       = "date";
-$timeslice         = "%Y-%m";
-
-$query = "select distinct DATE_FORMAT($date_column, \"$timeslice\") as date
+function getDates($table, $date_column, $timeslice)
+{
+  $query = "
+select distinct DATE_FORMAT($date_column, \"$timeslice\") as date
 from $table
      LEFT JOIN giqwm_fleet_driver as driver on $table.driver_id = driver.id
      LEFT JOIN giqwm_fleet_entity as dgroup on dgroup.id = driver.entity_id
      LEFT JOIN giqwm_fleet_entity as company on dgroup.parent_entity_id = company.id
      where driver.visible $where
-order by DATE_FORMAT($date_column, \"$timeslice\")";
-
-// Performing SQL query
-file_put_contents("/tmp/mysqllog.txt", $query . "\n", FILE_APPEND);
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-
-$cols = array();
-while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
-  foreach ($line as $column) {
-    array_push($cols, $column);
+order by DATE_FORMAT($date_column, \"$timeslice\")
+";
+  
+  // Performing SQL query
+  file_put_contents("/tmp/mysqllog.txt", $query . "\n", FILE_APPEND);
+  $result = mysql_query($query) or die('Query failed: ' . mysql_error());
+  
+  $cols = array();
+  while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    foreach ($line as $column) {
+      array_push($cols, $column);
+    }
   }
+  return $cols;
 }
+
+$dateColumns = getDates($table, $date_column, $timeslice);
+
+/* ----
+echo "request_columns=[$request_columns], sizeof=" . sizeof($request_columns);
+echo "groupby_columns=[$groupby_columns], sizeof=" . sizeof($groupby_columns);
+echo "alias_columns=[$alias_columns], sizeof=" . sizeof($alias_columns);
+---  */
 
 $query    = "";
 $subquery = "";
 $columns  = "";
+
+# Each aggregate column needs its own transpose query.
 foreach ($aggregate_columns as $aggcol) {
   
-  $subquery = "select $alias_columns, \"$aggcol\"";
-  foreach ($cols as $column) {
+  $subquery = "select ";
+  if ($alias_columns != "")
+    $subquery .= " $alias_columns,";
+  $subquery .= " \"$aggcol\"";
+  
+  foreach ($dateColumns as $column) {
     if (sizeof($columns) > 0) {
       $columns .= ",\n";
     }
     $columns .= "MAX(CASE WHEN time_period = \"$column\" THEN $aggcol END) as \"$column\"";
   }
   $subquery .= $columns;
+  
   $subquery .= " from
     (select $request_columns
       DATE_FORMAT($date_column, \"$timeslice\") as time_period,
@@ -131,10 +155,14 @@ foreach ($aggregate_columns as $aggcol) {
      LEFT JOIN giqwm_fleet_entity as dgroup on dgroup.id = driver.entity_id
      LEFT JOIN giqwm_fleet_entity as company on dgroup.parent_entity_id = company.id
      where driver.visible $where
-     group by $group_by_columns, time_period
-    ) as table_$aggcol
-    group by $alias_columns
-    ";
+     group by time_period
+     ";
+  if ($groupby_columns != "")
+    $subquery .= " , $groupby_columns ";
+  $subquery .= "
+    ) as table_$aggcol ";
+  if ($alias_columns != "")
+    $subquery .= " group by $alias_columns ";
   
   if ($query != "") {
     $query .= " UNION \n";
@@ -146,18 +174,20 @@ foreach ($aggregate_columns as $aggcol) {
 }
 
 
-$query = "
-select union_query.* from ($query) as union_query
-order by $alias_columns
-";
+$finalquery = "select ";
+if ($alias_columns != "")
+  $finalquery .= " $alias_columns, ";
+$finalquery .= " union_query.* from ($query) as union_query ";
+if ($alias_columns != "")
+  $finalquery .= " order by $alias_columns ";
 
-//echo $query;
-//return;
+echo $finalquery;
+// return;
 
 
 // Perform SQL query
-file_put_contents("/tmp/mysqllog.txt", $query . "\n", FILE_APPEND);
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+file_put_contents("/tmp/mysqllog.txt", $finalquery . "\n", FILE_APPEND);
+$result = mysql_query($finalquery) or die('Query failed: ' . mysql_error());
 
 #echo "<pre>";
 
