638aa904a69a8a0d44dcdba12b5c6a0e085a6650
kelvin <kelvin@09b8f181-b358-4790-9798-8c328cd25aca>
2015-11-17 04:54:54 +0000

    sorted drivers in the descending order of creation date. Fixed the unresolved reference error in javascript.

--- trunk/components/com_fleetmatrix/models/driver.php
+++ trunk/components/com_fleetmatrix/models/driver.php
@@ -9,32 +9,26 @@ class FleetMatrixModelDriver extends FleetMatrixModelBase
 {
     var $model_key = 'Driver';
 
-	public function getItem()
-	{
+    public function getItem() {
         parent::getItem();
 
-		if (!isset($this->item))
-		{
-			$id = $this->getState('driver.id');
+        if (!isset($this->item)) {
+            $id = $this->getState('driver.id');
 
-			$query = $this->_db->getQuery(true)
-				->from('#__fleet_driver as h')
-				->leftJoin('#__fleet_entity as c ON h.entity_id=c.id')
-				->select('h.*, c.name as entity')
-				->where('h.id=' . (int)$id);
+            $query = $this->_db->getQuery(true)
+                    ->from('#__fleet_driver as h')
+                    ->leftJoin('#__fleet_entity as c ON h.entity_id=c.id')
+                    ->select('h.*, c.name as entity')
+                    ->where('h.id=' . (int)$id);
 
             $this->_db->setQuery($query);
 
-			if (!$this->item = $this->_db->loadObject())
-			{
-				$this->setError($this->_db->getErrorMsg());
-			}
-			else
-			{
-                // success
-			}
-		}
-		return $this->item;
-	}
+            if (!$this->item = $this->_db->loadObject()) {
+                $this->setError($this->_db->getErrorMsg());
+            }
+        }
+
+        return $this->item;
+    }
 
 }
